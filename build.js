25a148e17af39036651f8c42098883242dc431b5
phiresky <phireskyde+git@gmail.com>
2017-04-25 19:27:15 +0200

    setup gh-pages branch

--- build.js
+++ build.js
@@ -3,6 +3,10 @@ const path = require('path');
 const cp = require('child_process');
 if (!fs.existsSync(path.join(__dirname, "build", ".git"))) {
     console.log("setting up worktree");
+    const cb = cp.spawnSync("git", "show-ref --verify --quiet refs/heads/gh-pages".split(" "));
+    if(cb.status !== 0) {
+        cp.spawnSync("git", ["branch", "--track", "gh-pages", "origin/gh-pages"]);
+    }
     cp.spawnSync("git", ["worktree", "prune"]);
     const ret = cp.execSync("git worktree add build gh-pages");
     console.log(ret.toString('utf8'));
